aa97dbbb9844da59a776f64cfca7e8d79f942b26
chirpdx <chir@pdx.edu>
2022-04-07 19:55:17 -0700

    needs tentative 3 states
    opcodes covered in unlock state, bidamount taken in lock state, comparison pending

--- hdl/bids22.sv
+++ hdl/bids22.sv
@@ -9,21 +9,12 @@
 ////////////////////////////////////////////////////////////////
 /*Queries and understanding::
 
-Start time when incorrect key is given, but where is the other use of timer? Does, 
+Start timer when incorrect key is given, but where is the other use of timer? Does, 
 Note: Timer is always active, but the inptopcode, is used for setting/updating the timer value.
 So, what should be default value of timer?
 
-C_start is not as strobe. When C_start is high bids can be placed.
-
-XYZ mask value can be cosidered as lower 3 bits of C_data
-
-Every clock the bid signal is high, bid charge will be deducted.
-
-Ready will be high when result is ready. ideally after C_start is deasserted.
-Difference between roundOver and ready?
-
-Does the bidAmt in same round needs to be in increasing order?
-
+// ack should be high when error occurs or not.
+Error is still there, and we give xbid, in this case xack should not be given
 Note: err are 2 bits
 
 */
@@ -46,8 +37,17 @@ module bids22(clk, reset_n, X_bidAmt, X_bid, X_retract,
 	output logic X_ack, X_win, Y_ack, Y_win, Z_ack, Z_win, ready, roundOver;
 	output logic [1:0] X_err, Y_err, Z_err, err;
 	output logic [31:0] X_balance, Y_balance, Z_balance;
-	
+	output logic [31:0] maxBid;
+
+// Internal Registers
 logic [31:0] X_value, Y_value, Z_value;
+logic [31:0] xtemp, ytemp, ztemp;
+logic [31:0] xcurr, ycurr, zcurr;
+logic [31:0] timer;
+logic [31:0] key;
+logic [2:0] mask;
+logic [31:0] bid_cost;
+
 enum logic[3:0] {	NoOp 		= 4'b0000,
 					Unlock		= 4'b0001,
 					Lock		= 4'b0010,
@@ -59,30 +59,153 @@ enum logic[3:0] {	NoOp 		= 4'b0000,
 					BidCharge	= 4'b1000
 					} Opcode;
 
-bit unlock_recognized = 0;
+logic unlock_recognized; // Lock Flag
 
 always_ff@(posedge clk)
 begin
 	if(reset_n == 0)
-begin
-	BidCharge <= 1;
-end
+	begin
+		unlock_recognized <= 1;
+		X_value <= '0;
+		Y_value <= '0;
+		Z_value <= '0;
+		mask <= 3'b111;
+		timer <= 32'hF;
+		key <= '0;
+		bid_cost <= 1;
+
+		{xtemp, ytemp, ztemp} <= '0;
+		{xcurr, ycurr, zcurr} <= '0;
+
+		ready <= 1'b0;
+		{X_win, Y_win, Z_win} <= '0;
+		err <= '0;
+		{X_ack, Y_ack, Z_ack} <= 0;
+		{X_balance, Y_balance, Z_balance} <= '0;
+		{X_err, Y_err, Z_err} <= '0;
+		maxBid <= '0;
+		roundOver <= '0;
+	end
 else
-		unique case(C_op)
-			NoOp : begin
-				X_balance = X_value;
+	ready <= 1'b1;
+	if(unlock_recognized == 1)  // Unlock state
+	begin
+		if(C_op == 0)
+		begin
+			key <= key;
+			X_value <= X_value;
+			Y_value <= Y_value;
+			Z_value <= Z_value;
+			xtemp <= X_value;
+			ytemp <= X_value;
+			ztemp <= X_value;
+			mask <= mask;
+			bid_cost <= bid_cost;
+			timer <= timer;
+		end
+		else if(C_op == 1)
+		begin
+			err <= 3'b010; //already unlocked
+		end
+		else if(C_op == 2)
+		begin
+			key <= C_data;
+			unlock_recognized <= 1'b1;   //needs to check if this works--going to lock
+		end
+		else if(C_op == 3)
+		begin
+			X_value <= C_data;
+			xtemp <= C_data;
+		end
+		else if(C_op == 4)
+		begin
+			Y_value <= C_data;
+			ytemp <= C_data;
+		end
+		else if(C_op == 5)
+		begin
+			Z_value <= C_data;
+			ztemp <= C_data;
+		end
+		else if(C_op == 6)
+			mask <= C_data[2:0];
+		else if(C_op == 7)
+			timer <= C_data;
+		else if(C_op == 8)
+			bid_cost <= C_data;
+		else
+			err <= 100; // invalid operation
+		if(C_start)
+			err<= 011; // cannot assert c_start when unlocked
+	
+	end
+	else if(unlock_recognized == 0) // Lock state
+	begin
+		if(C_start == 1)	// round start
+		begin
+			
+			if(mask[2] == 1 && X_bid == 1)
+				if((xtemp - X_bidAmt - bid_cost) >= 0)
+				begin
+					xcurr <= X_bidAmt;
+					xtemp <= xtemp - bid_cost;
+				end
+				else
+				begin
+					X_err <= 2'b10;		//insufficient funds
+					xtemp <= xtemp - bid_cost;		// confirm whether required
+				end
+			else if(mask[2] == 1 && X_bid == 0)
+				xcurr <= xcurr;
+			else if(mask[2] == 0 && X_bid == 1)
+			begin
+				X_err <= 2'b11;
+			end
+			else
+				X_err <= 2'b00;   // does it require xcurr also
+			
+			if(mask[1] == 1 && Y_bid == 1)
+				if((ytemp - Y_bidAmt - bid_cost) >= 0)
+				begin
+					ycurr <= Y_bidAmt;
+					ytemp <= ytemp - bid_cost;
+				end
+				else
+				begin
+					Y_err <= 2'b10;		//insufficient funds
+					ytemp <= ytemp - bid_cost;
+				end
+			else if(mask[1] == 1 && Y_bid == 0)
+				ycurr <= ycurr;
+			else if(mask[1] == 0 && Y_bid == 1)
+				Y_err <= 2'b11;
+			else
+				Y_err <= 2'b00;
+			
+			if(Z_bid == 1)
+				if((ztemp - Z_bidAmt - bid_cost) >= 0)
+				begin
+					zcurr <= Z_bidAmt;
+					ztemp <= ztemp - bid_cost;
+				end
+				else
+				begin
+					Z_err <= 2'b10;		//insufficient funds
+					ztemp <= ztemp - bid_cost;
+				end
+			else if(mask[0] == 1 && Z_bid == 0)
+				zcurr <= zcurr;
+			else if(mask[0] == 0 && Z_bid == 1)
+				Z_err <= 2'b11;
+			else
+				Z_err <= 2'b00;
+//if c_start becomes zero then you will go to result state
+		end
+			
+	end
+
 end
-			Unlock: if(C_data === unlock_key) unlock_recognized = 1; else unlock_recognized = 0;
-			Lock:
-			LoadX:
-			LoadY:
-			LoadZ:
-			SetXYZmask:
-			SetTimer:
-			BidCharge:
-			default:
-		endcase
-end
+
 
 endmodule: bids22
 	
